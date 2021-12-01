11db53074854aad43c2b98a5ae91892eb958b3d2
Jhon Med <jhonatan1medeiros@gmail.com>
2021-12-01 20:42:33 -0300

    Corrigindo erro

--- SpringTools/blogJhon/src/main/java/org/generation/blogJhon/controller/PostagemController.java
+++ SpringTools/blogJhon/src/main/java/org/generation/blogJhon/controller/PostagemController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,18 @@ public class PostagemController {
 	public ResponseEntity<List<Postagem>> GetAll(){
 		return ResponseEntity.ok(repository.findAll());
 	}
+	
+	// retornar uma postagem pelo id
+    @GetMapping("/{id}")
+    public ResponseEntity<Postagem> GetById(@PathVariable long id){
+        return repository.findById(id)
+                .map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+    }
+
+    // retornar uma postagem pelo titulo
+    @GetMapping("/titulo/{titulo}")
+    public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo){
+        return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
+    }
 	
 }
